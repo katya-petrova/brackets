acbc11ad56d335d7f9e7d25a4f5310522f35a7d0
katya-petrova <walki-disi@ya.ru>
2021-02-25 23:21:16 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stack = [];
+    bracketsConfig = bracketsConfig.flat().join('');
+    for (let i = 0; i < str.length; i = i + 1) {
+        let char = str[i];
+        let bracketIndex = bracketsConfig.indexOf(str[i]); 
+        if (bracketIndex % 2 === 0) {
+            stack.push(bracketIndex + 1);
+        } else {
+            if(stack.pop() !== bracketIndex) {
+        return false;
+        }
+    }
+    } 
+    return stack.length === 0;
 }
